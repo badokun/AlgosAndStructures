9802683d3810fc27321faeaa1372e70635923ef7
Henno van Rensburg <hennovanrensburg@gmail.com>
2017-03-04 12:09:25 +0900

    Adding missing items

---
+++ src/Algorithms/Sorting/DevideAndConquer/MergeSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Algorithms.Sorting.Linear;
+
+namespace Algorithms.Sorting.DevideAndConquer
+{
+    public class MergeSort
+    {
+        public static T[] Sort<T>(T[] source) where T : IComparable<T>
+        {
+            var bits = new List<T[]>(source.Length);
+            MakeArraysOfOne(source, bits);
+            
+            // Eventually we'll end up again with one item containing the reconstructed array
+            while (bits.Count > 1)
+            {
+                var inProgress = new List<T[]>();
+                for (int i = 0; i < bits.Count; i= i + 2)
+                {
+                    if (i + 1 > bits.Count-1)
+                    {
+                        inProgress.Add(bits[i]);
+                    }
+                    else
+                    {
+                        var joinedArray = CombineAndSort(bits[i], bits[i + 1]);
+                        inProgress.Add(joinedArray);
+                    }
+                }
+
+                bits = inProgress;
+            }
+
+            return bits[0];
+        }
+
+        private static void MakeArraysOfOne<T>(T[] source, List<T[]> bits) where T : IComparable<T>
+        {
+            bits.AddRange(source.Select(t => new T[] {t}));
+        }
+
+        private static T[] CombineAndSort<T>(T[] left, T[] right) where T : IComparable<T>
+        {
+            var result = new List<T>(left.Length + right.Length);
+            if (left[0].CompareTo(right[0]) < 0)
+            {
+                result.AddRange(left);
+                result.AddRange(right);
+            }
+            else
+            {
+                result.AddRange(right);
+                result.AddRange(left);
+            }
+
+            return BubbleSort.Sort(result.ToArray());
+        }
+    }
+}
---
+++ src/Algorithms/Sorting/Linear/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms.Sorting.Linear
+{
+    public class SelectionSort
+    {
+        public static T[] Sort<T>(T[] source) where T : IComparable<T>
+        {
+            var itemArray = source.ToArray();
+
+            for (int i = 0; i < itemArray.Length; i++)
+            {
+                int smallestItemIndex = i;
+                for (int j = i; j < itemArray.Length-1; j++)
+                {
+                    if (itemArray[j + 1].CompareTo(itemArray[smallestItemIndex]) < 0)
+                    {
+                        smallestItemIndex = j + 1;
+                    }
+                }
+                if (smallestItemIndex != 0 && smallestItemIndex != i)
+                {
+                    Shared.Move(itemArray, smallestItemIndex, i);
+                }
+            }
+
+            return itemArray;
+        }
+    }
+}
