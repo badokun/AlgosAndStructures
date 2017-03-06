dbdfb551b78fb7003638b85892b366268b674088
Henno van Rensburg <hennovanrensburg@gmail.com>
2017-03-06 10:00:08 +0900

    Removing Structures project

---
+++ src/Algorithms/Lists/MyLinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Lists
+{
+    public class MyLinkedList<T> : IEnumerable<T>
+    {
+        private MyLinkedListNode<T> _head;
+        private MyLinkedListNode<T> _current;
+
+        public void Add(T item)
+        {
+            if (_head == null)
+            {
+                _head = new MyLinkedListNode<T>() {Value = item};
+                _current = _head;
+            }
+            else
+            {
+                _current.Next = new MyLinkedListNode<T>() { Value = item };
+                _current = _current.Next;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            var current = _head;
+
+            var previous = current;
+            while (current != null)
+            {
+                if (current.Next != null)
+                {
+                    previous = current;
+                }
+                current = current.Next;
+            }
+            previous.Next = null;
+        }
+
+        public void RemoveFirst()
+        {
+            _head = _head.Next;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = _head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class MyLinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public MyLinkedListNode<T> Next { get; set; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ src/Tests/AlgorithmTests/Lists/LinkedListTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Lists;
+using NUnit.Framework;
+
+namespace AlgorithmTests.Lists
+{
+    [TestFixture]
+    public class LinkedListTests
+    {
+        [Test]
+        public void CanEnumerate()
+        {
+            // Arrange
+            var list = CreateList();
+
+            // Act
+            var items = list.ToList();
+
+            // Assert
+            Assert.AreEqual(3, items.Count);
+        }
+
+        [Test]
+        public void RemoveFirst()
+        {
+            // Arrange
+            var list = CreateList();
+
+            // Act
+            list.RemoveFirst();
+            var items = list.ToList();
+
+            // Assert
+            Assert.AreEqual(2, items.Count);
+            Assert.AreEqual(15, items[0]);
+            Assert.AreEqual(20, items[1]);
+        }
+
+        [Test]
+        public void RemoveLast()
+        {
+            // Arrange
+            var list = CreateList();
+
+            // Act
+            list.RemoveLast();
+            var items = list.ToList();
+
+            // Assert
+            Assert.AreEqual(2, items.Count);
+            Assert.AreEqual(10, items[0]);
+            Assert.AreEqual(15, items[1]);
+        }
+
+        private static MyLinkedList<int> CreateList()
+        {
+            var list = new MyLinkedList<int>();
+            list.Add(10);
+            list.Add(15);
+            list.Add(20);
+            return list;
+        }
+    }
+}
