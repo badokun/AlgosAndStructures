0829762753d6c4243984963295d5b444b9834336
Henno van Rensburg <hennovanrensburg@gmail.com>
2017-03-10 14:50:57 +0900

    Adding string calculator

---
+++ src/Algorithms/StringCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class StringCalculator
+    {
+        // Write a method to find the index of the first non-repeated character in a String
+        //      012345
+        // E.g. status -> 2
+
+        public static int FindIndexOfNonRepeated(string input)
+        {
+            List<char> characters = new List<char>();
+            characters.Add(input[0]);
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (characters.Contains(input[i]))
+                {
+                    characters.Remove(input[i]);
+                }
+                else
+                {
+                    characters.Add(input[i]);
+                }
+            }
+
+            var firstCharacter = characters.First();
+            return input.IndexOf(firstCharacter);
+        }
+    }
+}
---
+++ src/Tests/AlgorithmTests/StringCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms;
+using NUnit.Framework;
+
+namespace AlgorithmTests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("status", 2)]
+        [TestCase("stattusat", 5)]
+        public void FindIndexOfNonRepeated(string input, int index)
+        {
+            // Arrange
+
+            // Act
+            var result =StringCalculator.FindIndexOfNonRepeated(input);
+
+            // Assert
+            Assert.AreEqual(index, result);
+        }
+    }
+}
